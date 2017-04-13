9e8ea81ef130eb53038c97baabc4d7d86f30caea
asyrafsalim <ninjatrickz@gmail.com>
2017-04-13 17:53:49 +0800

    completed webchat init, TODO: callback for user session check

--- publish/scripts/main.js
+++ publish/scripts/main.js
@@ -1,38 +1,46 @@
 requirejs(["../botchat"], function (BotChat) {
-
+  console.log(localStorage);
   chatInit();
 
   function chatInit() {
+    var userDetails = {
+      id: '999',
+      name: 'anonymous user'
+    };
+    var botDetails = {
+      id: '1',
+      name: 'dbs bot'
+    };
     if (checkStorage()) {
       var chatConversationId = localStorage.getItem('chatConversationId');
       var chatToken = localStorage.getItem('chatToken');
 
-      if(!checkUserSession()) {
-        chatSession(chatToken);
+      //TODO call back for user session
+      var userSession = checkUserSession(chatConversationId, chatToken);
+      
+
+      if(userSession) {
+        console.log('session valid');
+        userDetails = userSession.user;
+        chatSession(chatToken, userDetails, botDetails)
       } else {
-        var userDetails = {
-          id: 'userid',
-          name: 'anonymous user'
-        };
-        var botDetails = {
-          id: 'botid',
-          name: 'dbs bot'
-        };
-        chatSession(chatToken);
+        console.log('session not valid');
+        chatSession(chatToken, userDetails, botDetails)
       }
     } else {
       var httpRequest = new XMLHttpRequest();
-      httpRequest.open('GET', 'https://dbs-chat-staging.herokuapp.com/token');
+      httpRequest.open('GET', 'https://dbs-chat-staging.herokuapp.com/bot/token');
       httpRequest.onload = function () {
         if (httpRequest.status === 200) {
           var chatTokenExchange = JSON.parse(httpRequest.responseText);
 
           localStorage.setItem('chatConversationId', chatTokenExchange.conversationId);
           localStorage.setItem('chatToken', chatTokenExchange.token);
-          chatSession(chatTokenExchange.token);
+
+          chatSession(chatTokenExchange.token, userDetails, botDetails)
         }
         else {
-          console.log('Request failed. Returned status of ' + httpRequest.status);
+          console.log('Request failed.');
           chatInit();
         }
       }
@@ -41,9 +49,35 @@ requirejs(["../botchat"], function (BotChat) {
   }
 
   function checkStorage() {
-    if (localStorage.getItem('chatConversationId' != null && localStorage.getItem('chatToken') != null)) {
+    if (localStorage.getItem('chatConversationId') != null && localStorage.getItem('chatToken') != null) {
       return true;
     }
+  }
+
+  function checkUserSession(chatConversationId, chatToken) {
+    // for testing include sessionMock
+    // var sessionMock = Math.random() >= 0.5;
+    var sessionMock = true;
+    var getUrl = 'https://dbs-chat-staging.herokuapp.com/bot/session'+'/?chatConversationId='+chatConversationId+'&chatToken='+chatToken+'&mock='+sessionMock;
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.open('GET', getUrl);
+    httpRequest.onload = function () {
+      if (httpRequest.status === 200) {
+        var userState = JSON.parse(httpRequest.responseText);
+        console.log(userState);
+        if (userState.hasOwnProperty('error') && userState.error == 'Unauthorized') {
+          return false;
+        } else {
+          return userState;
+        }
+      }
+      else {
+        console.log('Request failed.');
+        return false;
+      }
+    }
+    httpRequest.send();
   }
 
   function chatSession(chatToken, userDetails, botDetails) {
@@ -62,24 +96,10 @@ requirejs(["../botchat"], function (BotChat) {
     // localStorage.removeItem('chatToken');
   }
 
-  function checkUserSession(chatConversationId, chatToken) {
-    var httpRequest = new XMLHttpRequest();
-    var getUrl = 'https://dbs-chat-staging.herokuapp.com/user-state'+'/?chatConversationId='+chatConversationId+'&chatToken='+chatToken;
-    httpRequest.open('GET', getUrl);
-    httpRequest.onload = function () {
-      if (httpRequest.status === 200) {
-        var userState = JSON.parse(httpRequest.responseText);
-        if (userState.session) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-      else {
-        console.log('Request failed. Returned status of ' + httpRequest.status);
-        checkUserSession(chatConversationId, chatToken);
-      }
-    }
-    httpRequest.send();
+  // TODO
+  function ajaxCall(url, methodType, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(methodType, url, true);
+    xhr.send();
   }
 });
